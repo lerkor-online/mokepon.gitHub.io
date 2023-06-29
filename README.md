f1a29f356ac128f56ed6bc75bc81948104ff2f3d
lerkor-online <lerkor.online@gmail.com>
2023-06-28 23:18:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mokepon.gitHub.io
---
+++ server/index.js
@@ -0,0 +1,100 @@
+const express = require("express")
+const cors = require("cors")
+require('dotenv').config();
+
+const app = express()
+
+app.use(express.static('server'))
+app.use(cors())
+app.use(express.json())
+
+const jugadores = []
+
+class Jugador {
+    constructor(id) {
+        this.id = id
+    }
+    asignarMokepon(mokepon) {
+        this.mokepon = mokepon
+    }
+    actualizarPosicion(x, y) {
+        this.x = x
+        this.y = y
+    }
+    asignarAtaques(ataques) {
+        this.ataques = ataques
+    }
+}
+
+class Mokepon {
+    constructor(nombre) {
+        this.nombre = nombre
+    }
+}
+
+app.get("/unirse", (req, res) => {
+    const id = `${Math.random()}`
+
+    const jugador = new Jugador(id)
+
+    jugadores.push(jugador)
+
+    res.setHeader("Access-Control-Allow-Origin", "https://mokepon-git-hub-io.vercel.app" )
+
+    res.send(id)
+})
+
+app.post("/mokepon/:jugadorId", (req, res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const nombre = req.body.mokepon || ""
+    const mokepon = new Mokepon(nombre)
+
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+    
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].asignarMokepon(mokepon)
+    }
+    console.log(jugadores)
+    console.log(jugadorId)
+    res.end()
+})
+
+app.post("/mokepon/:jugadorId/posicion", (req,res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const x = req.body.x || 0
+    const y = req.body.y || 0
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+    console.log(jugadores)
+
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].actualizarPosicion(x, y)
+    }
+    const enemigos=jugadores.filter((jugador)=>jugadorId!==jugador.id)
+    res.send({
+        enemigos
+    })
+})
+
+app.post("/mokepon/:jugadorId/ataques", (req, res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const ataques = req.body.ataques || []
+
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+    
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].asignarAtaques(ataques)
+    }
+    res.end()
+})
+
+app.get("/mokepon/:jugadorId/ataques", (req, res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const jugador = jugadores.find((jugador) => jugador.id === jugadorId)
+    res.send({
+    ataques: jugador.ataques || []
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening at',process.env.PORT)
+})
